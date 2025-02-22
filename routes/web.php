afb828a6065390c7843a8ef71bc15f0347a4c964
RidhaLaribi <ridalaribi26@gmail.com>
2025-02-22 01:13:03 +0100

    zid /login wla /profil ll url bah ydkhlk lihon

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome')->with("id", null);
 });
+
+Route::get('/login', function () {
+    return view('login')->with("id", null);
+});
 Route::get('/profile', function () {
     return view('profile');
 });
